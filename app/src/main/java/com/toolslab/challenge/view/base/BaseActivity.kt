a98685387e7ecd0250e8ab30a7d574d9123d785a
code-schreiber <code-schreiber@users.noreply.github.com>
2018-08-31 18:15:02 +0200

    Adds BaseActivity

---
+++ app/src/main/java/com/toolslab/challenge/view/base/BaseActivity.kt
@@ -0,0 +1,28 @@
+package com.toolslab.challenge.view.base
+
+import android.annotation.SuppressLint
+import android.support.annotation.StringRes
+import com.toolslab.challenge.R
+import com.toolslab.challenge.base_mvp.BaseView
+import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
+
+@SuppressLint("Registered") // BaseActivity should not go in the manifest
+open class BaseActivity : DaggerAppCompatActivity(), BaseView {
+
+    @Inject
+    internal lateinit var uiMessenger: UiMessenger
+
+    override fun showNoConnectionError() {
+        showMessage(R.string.error_no_connection)
+    }
+
+    override fun showDefaultError() {
+        showMessage(R.string.error_default)
+    }
+
+    internal fun showMessage(@StringRes id: Int) {
+        uiMessenger.showMessage(this, id)
+    }
+
+}
