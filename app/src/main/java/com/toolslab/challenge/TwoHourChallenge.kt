e0f911fdb27f75b2a37b1477f0cb49fb1561e4ae
code-schreiber <code-schreiber@users.noreply.github.com>
2018-08-31 18:15:30 +0200

    Adds app singleton

---
+++ app/src/main/java/com/toolslab/challenge/TwoHourChallenge.kt
@@ -0,0 +1,31 @@
+package com.toolslab.challenge
+
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.toolslab.challenge.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+class TwoHourChallenge : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Fresco.initialize(this)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        val repositoryComponent = DaggerRepositoryComponent.builder().build()
+        return DaggerAppComponent
+                .builder()
+                .create(this)
+                .repositoryComponent(repositoryComponent)
+                .build()
+    }
+
+}
